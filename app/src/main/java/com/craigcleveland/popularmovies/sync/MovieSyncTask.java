eb9fc1b988152c281c821e6d139b8abcc0f46dff
Craig Cleveland <craig.cleveland@mgail.com>
2017-12-05 13:12:43 -0500

    Cleaned up Exception style in MovieSyncTask

--- app/src/main/java/com/craigcleveland/popularmovies/sync/MovieSyncTask.java
+++ app/src/main/java/com/craigcleveland/popularmovies/sync/MovieSyncTask.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.widget.Toast;
 
 import com.craigcleveland.popularmovies.R;
 import com.craigcleveland.popularmovies.data.MovieContract;
@@ -12,6 +13,9 @@ import com.craigcleveland.popularmovies.data.MovieProvider;
 import com.craigcleveland.popularmovies.utilities.MovieDBJsonUtils;
 import com.craigcleveland.popularmovies.utilities.NetworkUtils;
 
+import org.json.JSONException;
+
+import java.io.IOException;
 import java.net.URL;
 
 /**
@@ -42,59 +46,59 @@ public class MovieSyncTask {
 
         }
 
-        try {
-            URL movieRequestUrl = NetworkUtils.buildTMDBURL(
-                    sortType,
-                    context.getString(R.string.tmdbAPIKey),
-                    -1);
+        URL movieRequestUrl = NetworkUtils.buildTMDBURL(
+                sortType,
+                context.getString(R.string.tmdbAPIKey),
+                -1);
 
-            String jsonMovieResponse = NetworkUtils.getResponseFromHttpUrl(movieRequestUrl);
+        String jsonMovieResponse = getJsonResponse(movieRequestUrl, context);
 
-            ContentValues[] movieValues = MovieDBJsonUtils
-                    .getMovieContentValuesFromJson(jsonMovieResponse);
+        if (jsonMovieResponse != null) {
+            try {
+                ContentValues[] movieValues = MovieDBJsonUtils
+                        .getMovieContentValuesFromJson(jsonMovieResponse);
+                if (movieValues != null && movieValues.length != 0) {
+                    ContentResolver movieCR = context.getContentResolver();
+                    movieCR.delete(MovieContract.MovieEntry.MOVIE_CONTENT_URI, null, null);
 
-            if (movieValues != null && movieValues.length != 0) {
-                ContentResolver movieCR = context.getContentResolver();
-                movieCR.delete(MovieContract.MovieEntry.MOVIE_CONTENT_URI, null, null);
-
-                movieCR.bulkInsert(MovieContract.MovieEntry.MOVIE_CONTENT_URI, movieValues);
-
+                    movieCR.bulkInsert(MovieContract.MovieEntry.MOVIE_CONTENT_URI, movieValues);
+                }
+            } catch (JSONException e) {
+                // This should only happen if data coming back is bad or programming error
+                // Allowing crash
+                e.printStackTrace();
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-
     }
 
     synchronized public static void syncTrailers(Context context, int movie_id) {
 
-        try {
-            URL movieRequestUrl = NetworkUtils.buildTMDBURL(
-                    MovieProvider.TRAILERS_LIST,
-                    context.getString(R.string.tmdbAPIKey),
-                    movie_id);
+        URL movieRequestUrl = NetworkUtils.buildTMDBURL(
+                MovieProvider.TRAILERS_LIST,
+                context.getString(R.string.tmdbAPIKey),
+                movie_id);
 
-            String jsonMovieResponse = NetworkUtils.getResponseFromHttpUrl(movieRequestUrl);
+        String jsonMovieResponse = getJsonResponse(movieRequestUrl, context);
 
-            ContentValues[] trailerValues = MovieDBJsonUtils
-                    .getTrailerContentValuesFromJson(jsonMovieResponse);
+        if (jsonMovieResponse != null) {
+            try {
+                ContentValues[] trailerValues = MovieDBJsonUtils
+                        .getTrailerContentValuesFromJson(jsonMovieResponse);
 
-            if (trailerValues != null && trailerValues.length != 0) {
-                ContentResolver trailerCR = context.getContentResolver();
-                trailerCR.delete(MovieContract.MovieEntry.TRAILER_CONTENT_URI,
-                        null, null);
+                if (trailerValues != null && trailerValues.length != 0) {
+                    ContentResolver trailerCR = context.getContentResolver();
+                    trailerCR.delete(MovieContract.MovieEntry.TRAILER_CONTENT_URI,
+                            null, null);
 
-                trailerCR.bulkInsert(
-                        MovieContract.MovieEntry.TRAILER_CONTENT_URI, trailerValues);
-
+                    trailerCR.bulkInsert(
+                            MovieContract.MovieEntry.TRAILER_CONTENT_URI, trailerValues);
+                }
+            } catch (JSONException e) {
+                // This should only happen if data coming back is bad or programming error
+                // Allowing crash
+                e.printStackTrace();
             }
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-
-
     }
 
     synchronized public static void syncReviews(Context context, int movie_id) {
@@ -114,17 +118,33 @@ public class MovieSyncTask {
                 ContentResolver reviewCR = context.getContentResolver();
                 reviewCR.delete(MovieContract.MovieEntry.REVIEWS_CONTENT_URI,
                         null, null);
-
                 reviewCR.bulkInsert(
                         MovieContract.MovieEntry.REVIEWS_CONTENT_URI, reviewValues);
-
             }
 
         } catch (Exception e) {
+            // This should only happen if data coming back is bad or programming error
+            // Allowing crash
             e.printStackTrace();
         }
-
-
     }
+
+    private static String getJsonResponse(URL requestURL, Context context) {
+        String response;
+
+        try {
+            response = NetworkUtils.getResponseFromHttpUrl(requestURL);
+        } catch (IOException e) {
+            // This may occur if there's network is not available/disrupted, a message
+            // will be displayed, but and original data retained.
+            Toast.makeText(context,
+                    "Unable to get response from TMDB",
+                    Toast.LENGTH_LONG).show();
+            response = null;
+        }
+
+        return response;
+    }
+
 
 }
